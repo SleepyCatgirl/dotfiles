fe3a433b0a25260c369ee6134a40c60e0958e4e4
kurisu <shoukaku00@gmail.com>
2021-12-02 17:23:09 +0000

    Nix update

--- nix/system/configuration.nix
+++ nix/system/configuration.nix
@@ -3,7 +3,6 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -18,7 +17,11 @@
 
   networking.hostName = "nix"; # Define your hostname.
   networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
-
+  services.mullvad-vpn.enable = true;
+  programs.gamemode.enable = true;
+  networking.firewall.checkReversePath = "loose";
+  networking.wireguard.enable = true;
+  networking.iproute2.enable = true;
   nix.autoOptimiseStore = true; # Optimize for space
   networking.networkmanager.enable = true;	
   # Set your time zone.
@@ -39,6 +42,31 @@
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
   # };
+  #
+  # fonts
+  #
+  i18n.defaultLocale = "en_US.UTF-8";
+  fonts.fonts = with pkgs; [
+    ipafont
+    kochi-substitute
+    source-code-pro
+    carlito
+    dejavu_fonts
+  ];
+  fonts.fontconfig.defaultFonts = {
+    monospace = [
+      "DejaVu Sans Mono"
+      "IPAGothic"
+    ];
+    sansSerif = [
+      "DejaVu Sans"
+      "IPAPGothic"
+    ];
+    serif = [
+      "DejaVu Serif"
+      "IPAPMincho"
+    ];
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -48,25 +76,16 @@
 #  services.logind.lidSwitchDocked = "suspend";
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.startx.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.displayManager.startx.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome.gnome-music pkgs.evince pkgs.gnome.gnome-characters pkgs.gnome.totem
                                       ];
-  services.xserver.windowManager.xmonad = {
-    enable = true;
-    enableContribAndExtras = true;
-    extraPackages = haskellPackages: [
-      haskellPackages.xmonad-contrib
-      haskellPackages.xmonad-extras
-      haskellPackages.xmonad
-    ];
-  };
- services.xserver.windowManager.bspwm.enable = true; 
+ services.xserver.windowManager.bspwm.enable = true;
 
   zramSwap = {
     enable = true;
-    memoryPercent = 50;
+    memoryPercent = 110;
     numDevices = 1;
   };
   # Configure keymap in X11
@@ -104,43 +123,56 @@ services.pipewire = {
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.senchou = {
      isNormalUser = true;
-     extraGroups = [ "wheel" "networkmanage" "audio" "video" ]; # Enable ‘sudo’ for the user.
+     extraGroups = [ "wheel" "networkmanage" "audio" "video" "libvirtd" ]; # Enable ‘sudo’ for the user.
      home = "/home/senchou";
    };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     vim emacs ripgrep coreutils fd clang sqlite # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget git unzip # Basic tools
-     vulkan-tools google-chrome discord pulsemixer # web browser, chat, audio mixer
-     clojure clojure-lsp leiningen # Clojure dev
-     gcc gdb # C++ Dev
-     haskell-language-server ghc # Haskell dev
-     xmobar dmenu pywal polybarFull bspwm sxhkd # WM aesthics
-     gnome.gnome-tweaks # Gnome aesthics
-     openvpn # Uni.
 
-     scrot xclip # screenshoting and putting them in copy paste
-     nethack cool-retro-term # gaming
-     angband crawl           # gameeeess
-     steam-run-native        # FHS enviroment
-     wineWowPackages.staging # WINE
-     (winetricks.override {wine = wineWowPackages.staging;})
-     texlive.combined.scheme-full
-     mpv play-with-mpv yt-dlp # Download youtube and play em
-     git-crypt # sensitive information like GPG keys via .git
-     piper # logitech libratbagd GTK frontend
-           # For configuring them
-   ];
+   environment.systemPackages = with pkgs;
+        let
+          R-with-pkgs = rWrapper.override{packages = with rPackages; [ggplot2 tidyverse];};
+        in
+          [
+            vim emacs ripgrep coreutils fd clang sqlite # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+            wget git unzip # Basic tools
+            vulkan-tools google-chrome discord pulsemixer # web browser, chat, audio mixer
+            clojure clojure-lsp leiningen # Clojure dev
+            gcc gdb # C++ Dev
+            haskell-language-server ghc # Haskell dev
+            dmenu pywal polybarFull bspwm sxhkd # WM aesthics
+            gnome.gnome-tweaks # Gnome aesthics
+            openvpn # Uni.
+            mullvad-vpn # VPN
+            openjdk # Java run
+            wireguard # VPN
+            qbittorrent # Torrenting
+            R-with-pkgs
+            scrot xclip # screenshoting and putting them in copy paste
+            nethack cool-retro-term # gaming
+            angband crawl           # gameeeess
+            steam-run-native        # FHS enviroment
+            wineWowPackages.staging # WINE
+            winetricks
+            texlive.combined.scheme-full
+            mpv play-with-mpv yt-dlp # Download youtube and play em
+            git-crypt # sensitive information like GPG keys via .git
+            piper # logitech libratbagd GTK frontend
+            # For configuring them
+            virglrenderer # 3D Render for VMs
+            cling
+            anki # learning
+          ];
 
    programs.steam.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
    programs.gnupg.agent = {
-     enable = false;
-     enableSSHSupport = false;
+     enable = true;
+     enableSSHSupport = true;
+     pinentryFlavor = "curses";
    };
 
   # List services that you want to enable:
@@ -157,8 +189,10 @@ services.pipewire = {
   services.ratbagd.enable = true;
   #ZFS
   boot.kernelParams = [ "zfs.zfs_arc_max=326870912" "nohibernate" ]; # ZFS hibernating issue
+  boot.kernelModules = [ "kvm-intel" ]; # kvm
   boot.initrd.supportedFilesystems = ["zfs"];
   boot.supportedFilesystems = ["zfs"];
+boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
 ''; # zfs already has its own scheduler. without this my(@Artturin) computer froze for a second when i nix build something.
@@ -173,12 +207,23 @@ services.pipewire = {
   system.stateVersion = "21.05"; # Did you read the comment?
   # Hardwar accel etc
   services.xserver.videoDrivers = [ "amdgpu" ];
+  boot.initrd.kernelModules = ["amdgpu"];
   hardware.firmware = with pkgs; [
 	firmwareLinuxNonfree
 	];
+  virtualisation.libvirtd.enable = true;
   hardware.opengl = {
 	enable = true;
 	driSupport32Bit = true;
-};
-}
+  };
 
+
+
+  # Japanese input
+  # fcitx as IME
+  i18n.inputMethod = {
+    enabled = "fcitx";
+    # mozc
+    fcitx.engines = with pkgs.fcitx-engines; [mozc];
+  };
+}
